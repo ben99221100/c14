04d6754e2ae0cc1aaf39cd2e7c855f24b2c34395
ben99221100 <134168955+ben99221100@users.noreply.github.com>
2023-06-18 12:43:22 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+function lion() {
+    let lion = new Audio('lion.wav')
+    lion.play()
+}
+
+function dog() {
+    let dog = new Audio('dog.wav')
+    dog.play()
+}
+
+function horse() {
+    let horse = new Audio('https://www.w3schools.com/html/horse.ogg')
+    horse.play()
+}
